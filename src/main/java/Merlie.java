167d9b54290cba1dc9b904b83b96fc20cb2449da
Lyris Ng <lyrisng@u.nus.edu>
2025-08-22 00:50:08 +0800

    Rename Merlie to Merlie the Merlion

--- src/main/java/Merlie.java
+++ src/main/java/Merlie.java
@@ -2,7 +2,7 @@ public class Merlie {
     public static void main(String[] args) {
 
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Merlie\nWhat can I do for you?");
+        System.out.println("Hello! I'm Merlie the Merlion\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
